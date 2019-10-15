dee22db222e7da1ee10e9551c76f822bb5af66d3
maorRoz <andory@post.bgu.ac.il>
2019-10-15 14:05:31 +0300

    added button tests

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,8 +1,44 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Button } from './Button';
+import { Fakes } from '../../../test-utils/Fakes';
 
 describe('<Button>', () => {
+  const childElement = <div id="child" />;
+  test('self closing', () => {
+    const expectedText = Fakes.string();
+
+    const wrapper = shallow(<Button text={expectedText} onClick={jest.fn()} />);
+
+    const { children } = wrapper.props();
+
+    expect(children).toEqual(expectedText);
+  });
+
+  test('non-self-closing', () => {
+    const wrapper = shallow(
+      <Button onClick={jest.fn()}>{childElement}</Button>
+    );
+
+    const { children } = wrapper.props();
+
+    expect(children).toEqual(childElement);
+  });
+
+  test('non-self-closing - text override children', () => {
+    const expectedText = Fakes.string();
+
+    const wrapper = shallow(
+      <Button text={expectedText} onClick={jest.fn()}>
+        {childElement}
+      </Button>
+    );
+
+    const { children } = wrapper.props();
+
+    expect(children).toEqual(expectedText);
+  });
+
   test('onClick', () => {
     const onClickFn = jest.fn();
     const wrapper = shallow(<Button onClick={onClickFn} />);
